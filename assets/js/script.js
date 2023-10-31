478e019a25ca258ca9984654440855e821fbf3e2
DBrush <coldnebraska@gmail.com>
2023-10-30 20:43:12 -0500

    added flight api function for greg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 let token = ""
+let hotel = {
+  city: "AUS", //user input 
+  name: [],
+  radius: [],
+  rating: []
+}
 
 function getHotelToken () {
     let requestTokenURL = "https://test.api.amadeus.com/v1/security/oauth2/token"
     fetch(requestTokenURL, {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        body: 'grant_type=client_credentials&client_id={client key}&client_secret={secret key}'
+        body: 'grant_type=client_credentials&client_id=guryqLQUIRoKA4EyXNaj5uAeyAG1pG22&client_secret=byRpyX39LG4bx9Ap'
     })
     .then(function (response) {
         return response.json()
@@ -14,7 +20,26 @@ function getHotelToken () {
         token = data.access_token
       })
       .then(function () {
-        let requestUrl = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=PAR&radius=5&radiusUnit=KM&hotelSource=ALL"
+        let requestUrl = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=" + hotel.city
+        fetch(requestUrl, {
+            method: "GET",
+            headers: {'Authorization': 'Bearer ' + token}
+        })
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+            for (i = 0; i < data.meta.count; i++) {
+              hotel.name.push(data.data[i].name)
+              hotel.radius.push(data.data[i].distance.value + " KM")
+              hotel.rating.push(data.data[i].rating + " Stars")
+            }
+            console.log(hotel)
+        })
+      })
+      .then(function () {
+        let requestUrl = "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=BKK&departureDate=2023-12-02&adults=1&nonStop=false&max=250"
         fetch(requestUrl, {
             method: "GET",
             headers: {'Authorization': 'Bearer ' + token}
@@ -26,6 +51,6 @@ function getHotelToken () {
             console.log(data)
         })
       })
-}
+    }
 
 getHotelToken()
