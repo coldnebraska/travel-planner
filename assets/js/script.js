8d9ed2faf11e77d646eeb9a38e933821013f9422
DBrush <coldnebraska@gmail.com>
2023-11-02 11:36:23 -0500

    added save button to each hotel div. updated readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let token = ""
+let button = ""
 const hotel = {
-  iata: [],
+  iata: "",
   name: [],
   radius: [], 
   rating: []
@@ -8,7 +9,12 @@ const hotel = {
 const userInput = {
   city: "austin",
   region: "texas",
-  radius: 10 - 1
+  radius: 5 - 1 // !recommended 5 mi (prevent 100+ hotel list)
+}
+
+const savedItems = {
+  hotel: [],
+  flight: []
 }
 
 function searchIataCode() {
@@ -24,7 +30,7 @@ function searchIataCode() {
     console.log(data)
     for (i = 0; i < data.length; i++) {
       if (data[i].iata != "") {
-        hotel.iata.push(data[i].iata)
+        hotel.iata = data[i].iata
       }
     }
     console.log(hotel.iata)
@@ -49,23 +55,23 @@ function getHotelToken () {
         token = data.access_token
       })
       .then(function () {
-        const requestUrl = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=" + hotel.iata + "&radius=" + userInput.radius + "&radiusUnit=MILE" + "&ratings=5,4,3,2"
-        fetch(requestUrl, {
-            method: "GET",
-            headers: {'Authorization': 'Bearer ' + token}
+        const hotelUrl = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=" + hotel.iata + "&radius=" + userInput.radius + "&radiusUnit=MILE" + "&ratings=5,4,3,2"
+        fetch(hotelUrl, {
+          method: "GET",
+          headers: {'Authorization': 'Bearer ' + token}
         })
         .then(function(response){
-            return response.json()
+          return response.json()
         })
         .then(function(data){
-            console.log(data)
-            createHotelList(data)
-          })
+          console.log(data)
+          createHotelList(data)
         })
+      })
 
       .then( function () {
-        const requestUrl = "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=" + hotel.iata + "&destinationLocationCode=PAR&departureDate=2023-12-02&adults=1"
-        fetch(requestUrl, {
+        const flightUrl = "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=" + hotel.iata + "&destinationLocationCode=PAR&departureDate=2023-12-02&adults=1"
+        fetch(flightUrl, {
             method: "GET",
             headers: {'Authorization': 'Bearer ' + token}
         })
@@ -80,16 +86,15 @@ function getHotelToken () {
 
 function createHotelList(data) {
   const hotelList = $("#hotel-list") // !jquery to the hotel list div
-  
   for (i = 0; i < data.data.length; i++) {
     const createDiv = document.createElement("div")
     const createHeader = document.createElement("h1")
     const createPara = document.createElement("p")
     const createStar = document.createElement("p")
+    const saveButton = document.createElement("button")
 
     // create a hotel div for each returned hotel
     createDiv.setAttribute("class", "hotel")
-    createDiv.setAttribute("hotelID", data.data[i].hotelId)
     hotelList.append(createDiv)
     
     // add rating stars
@@ -119,5 +124,15 @@ function createHotelList(data) {
     hotelList.children().eq(i).append(createPara)
     createStar.textContent = hotel.rating[i]
     hotelList.children().eq(i).append(createStar)
+
+    // append save button to each div
+    saveButton.textContent =  "💾 Save"
+    saveButton.setAttribute("id", i)
+    hotelList.children().eq(i).append(saveButton)
   }
-} 
+  $("button[id]").click(function(data) {
+    console.log(this.id)
+    savedItems.hotel.push(data.data[this.id].name)
+    console.log(savedItems)
+  })
+}
