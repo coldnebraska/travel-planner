1dc62785b7f3c9d8ef8fcdba3daa64d0a6ee770c
Greg Yerkes <gyerkes@rajant.com>
2023-11-02 22:54:23 -0400

    updated js code to display data on html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,9 @@ let flights = {
     returnAirline: [],
     departFlightNumber: [],
     returnFlightNumber: [],
-    cost: []   
+    numPassengers: [],
+    cost: [] ,
+    button: []  
 }
 
 function getHotelToken () {
@@ -51,7 +53,7 @@ function getHotelToken () {
         })
       })
       .then(function () {
-        let requestUrl = "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=PHL&destinationLocationCode=ORD&departureDate=2023-11-15&returnDate=2023-11-17&adults=1&nonStop=true&currencyCode=USD&max=25"
+        let requestUrl = "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=PHL&destinationLocationCode=ORD&departureDate=2023-11-15&returnDate=2023-11-17&adults=2&nonStop=true&currencyCode=USD&max=25"
         fetch(requestUrl, {
             method: "GET",
             headers: {'Authorization': 'Bearer ' + token}
@@ -61,23 +63,27 @@ function getHotelToken () {
         })
         .then(function(data){
             console.log(data)
-            const flightOptions = $("#flightChoice")
+            const flightOptions = $(".options")
+            // const flightSave = $(".button")
             for(i = 0; i < data.data.length; ++i) {
 
-                const flightDiv = document.createElement("div");
-                flightDiv.setAttribute("class", "Flights");
-                flightOptions.append(flightDiv);
+                // const flightDiv = document.createElement("div");
+                // flightDiv.setAttribute("class", "Flights");
+                // flightOptions.append(flightDiv);
                 
                 const departFlightTime = document.createElement('h2');
                 const arrivalFlightTime = document.createElement('h2');
                 const returnDepartTime = document.createElement('h2');
                 const returnArrivalTime = document.createElement('h2');
                 const departAirlineCode = document.createElement('h2');
-                const returnAirlineCode = document.createElement('h2'); 
-                const departFlightNumber = document.createElement('h2');
-                const returnFlightNumber = document.createElement('h2');
+                // const returnAirlineCode = document.createElement('h2'); 
+                // const departFlightNumber = document.createElement('h2');
+                // const returnFlightNumber = document.createElement('h2');
+                const passengers = document.createElement('h2')
                 const flightPrice = document.createElement('h2');
-                const saveButton = document.createElement('button');
+                const flightSave = document.createElement('button');
+
+            
 
 
                 flights.departTime.push(data.data[i].itineraries[0].segments[0].departure.at)
@@ -89,27 +95,37 @@ function getHotelToken () {
                 flights.departFlightNumber.push(data.data[i].itineraries[0].segments[0].number)
                 flights.returnFlightNumber.push(data.data[i].itineraries[1].segments[0].number)
                 flights.cost.push(data.data[i].price.total)
+                flights.numPassengers.push(data.data[i].travelerPricings.length)
+                
 
                 departFlightTime.textContent = flights.departTime[i];
-                flightOptions.children().eq(i).append(departFlightTime);
+                flightOptions.children().eq(0).append(departFlightTime);
                 arrivalFlightTime.textContent = flights.arrivalTime[i];
-                flightOptions.children().eq(i).append(arrivalFlightTime);
+                flightOptions.children().eq(1).append(arrivalFlightTime);
                 returnDepartTime.textContent = flights.returnDepartTime[i];
-                flightOptions.children().eq(i).append(returnDepartTime);
+                flightOptions.children().eq(2).append(returnDepartTime);
                 returnArrivalTime.textContent = flights.returnArrivalTime[i];
-                flightOptions.children().eq(i).append(returnArrivalTime);
+                flightOptions.children().eq(3).append(returnArrivalTime);
                 departAirlineCode.textContent = flights.departAirline[i];
-                flightOptions.children().eq(i).append(departAirlineCode);
-                returnAirlineCode.textContent = flights.returnAirline[i];
-                flightOptions.children().eq(i).append(returnAirlineCode);
-                departFlightNumber.textContent = flights.departFlightNumber[i];
-                flightOptions.children().eq(i).append(departFlightNumber);
-                returnFlightNumber.textContent = flights.returnFlightNumber[i];
-                flightOptions.children().eq(i).append(returnFlightNumber);
+                flightOptions.children().eq(4).append(departAirlineCode);
+                // returnAirlineCode.textContent = flights.returnAirline[i];
+                // flightOptions.children().eq(i).append(returnAirlineCode);
+                // departFlightNumber.textContent = flights.departFlightNumber[i];
+                // flightOptions.children().eq(i).append(departFlightNumber);
+                // returnFlightNumber.textContent = flights.returnFlightNumber[i];
+                // flightOptions.children().eq(i).append(returnFlightNumber);
                 flightPrice.textContent = flights.cost[i];
-                flightOptions.children().eq(i).append(flightPrice);
+                flightOptions.children().eq(5).append(flightPrice);
+                passengers.textContent = flights.numPassengers[i];
+                flightOptions.children().eq(6).append(passengers);
+
+                flightSave.textContent = "💾 Save";
+                flightOptions.children().eq(7).append(flightSave);
 
             }
+
+           
+
 
 
 
